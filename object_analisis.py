f348d1dcd5ec2b97044dd5e67a66037c45ebdcb1
hudi-hub <huongdinh@hotmail.com>
2022-07-23 14:46:35 +0800

    "9. Add comments and descriptions"

--- object_analisis.py
+++ object_analisis.py
@@ -1,6 +1,12 @@
 import json
 import sys
 
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+# Functionn aboundant to find the highest total of objects by type
+# Taking an object array with elements are dictionary as input                                               
+# Counting the total of stars, galaxies, supernovae, frbs (assumption is fast radio burst - doublecheck)
+# Return a string which is the name of the type of the which has the highest count.
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 def aboundant(objects):
     # asign initiate variables in one line
     sum_stars = sum_galaxies = sum_supernovae = sum_frbs = 0
@@ -27,11 +33,17 @@ def aboundant(objects):
         return 'frbs'
 
 
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+# Function farthest to find the object with highest redshift
+# Taking an object array with elements are dictionary as input                                               
+# Return an object which has the highest redshift
+# If many objects have the same highest redshift. It will return the first one to be found.
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 def farthest(objects):
     # initiate value is the first object is highest to reduce calculation and logical condition
-    highest_redshift = objects[0]["redshift"] 
+    highest_redshift = None
     for o in objects:
-        if o["redshift"] > highest_redshift:
+        if (highest_redshift == None) or (o["redshift"] > highest_redshift):
             highest_redshift = o["redshift"]
     for o in objects:
         if o["redshift"] == highest_redshift:
@@ -39,6 +51,7 @@ def farthest(objects):
 
 
 def main():
+    # Some example inputs to test funtions
     input = """
     [
         {
@@ -84,12 +97,18 @@ def main():
     ]
     """
 
+    input3 = """
+    []
+    """
+
     print(aboundant(json.loads(input)))
     print(farthest(json.loads(input)))
 
     # little examples 
     print(aboundant(json.loads(input1)))
     print(farthest(json.loads(input2)))
+    print(aboundant(json.loads(input3)))
+    print(farthest(json.loads(input3)))
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
